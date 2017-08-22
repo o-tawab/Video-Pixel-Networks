9c1f1cff43b072217ae598bfdf1f0fa02c4b9307
Omar ABDELTAWAB (EXT) <omar.abdeltawab.ext@valeo.com>
2017-08-22 13:41:38 +0200

    incomplete model

--- layers.py
+++ layers.py
@@ -1,60 +1,46 @@
 import tensorflow as tf
+import numpy as np
 
 
-class MultiplicativeUnit(tf.nn.rnn_cell.BasicLSTMCell):
-    def __init__(self, num_units, w_hv, w_hz, w_vh, w_va, forget_bias=1.0,
-                 state_is_tuple=True, activation=None, reuse=None):
-        super(tf.nn.rnn_cell.BasicLSTMCell, self).__init__(_reuse=reuse)
+def masked_conv2d(inputs,
+                  in_channels,  # num of channels in input
+                  filters,
+                  kernel_size,
+                  mask_type,  # A, B or None
+                  channels_masked,
+                  padding='valid',
+                  activation=None,
+                  kernel_initializer=None,
+                  name=None):
 
-        self._num_units = num_units
-        self._forget_bias = forget_bias
-        self._state_is_tuple = state_is_tuple
-        self._activation = activation
+    with tf.variable_scope(name):
+        weights_shape = (kernel_size, kernel_size, in_channels, filters)
+        weights = tf.get_variable("weights", weights_shape,
+                                  tf.float32, kernel_initializer)
 
-        self._w_hv = w_hv
-        self._w_hz = w_hz
-        self._w_vh = w_vh
-        self._w_va = w_va
+        if mask_type is not None:
+            center_h = kernel_size // 2
+            center_w = kernel_size // 2
 
-    def __call__(self, x, h, a, scope=None):
-        with tf.variable_scope(self._scope or self.__class__.__name__):
-            previous_memory, previous_output = h
+            mask = np.ones(weights_shape, dtype=np.float32)
 
-            v = tf.matmul(self._w_vh, tf.transpose(previous_output, (1, 0))) * tf.matmul(self._w_va,
-                                                                                         tf.transpose(a, (1, 0)))
-            w_v = tf.matmul(self._w_hv, v)
-            iv, fv, ov, cv = tf.split(w_v, 4, axis=0)
-            w_z = tf.matmul(self._w_hz, tf.transpose(x))
-            iz, fz, oz, cz = tf.split(w_z, 4, axis=0)
+            mask[center_h, center_w + 1:, :channels_masked, :] = 0.
+            mask[center_h + 1:, :, :channels_masked, :] = 0.
 
-            i = tf.sigmoid(iv + iz)
-            f = tf.sigmoid(fv + fz)
-            o = tf.sigmoid(ov + oz)
-            memory = f * tf.transpose(previous_memory, (1, 0)) + i * tf.tanh(cv + cz)
-            output = o * tf.tanh(memory)
+            if mask_type == 'A':
+                mask[center_h, center_w, :channels_masked, :] = 0.
 
-        return tf.transpose(output, (1, 0)), tf.contrib.rnn.LSTMStateTuple(tf.transpose(memory, (1, 0)),
-                                                                           tf.transpose(output, (1, 0)))
+            weights *= tf.constant(mask, dtype=tf.float32)
+
+        outputs = tf.nn.conv2d(inputs, weights, [1, 1, 1, 1], padding=padding, name='outputs')
+
+        biases = tf.get_variable("biases", [filters, ], tf.float32, tf.zeros_initializer())
+
+        outputs = tf.nn.bias_add(outputs, biases, name='outputs_plus_b')
+
+        if activation:
+            outputs = activation(outputs, name='outputs_with_fn')
+
+        return outputs
 
 
-def actionlstm_cell(scope, x, h, a, num_units, input_shape, action_dim,
-                    initializer=tf.contrib.layers.xavier_initializer(), forget_bias=1.0,
-                    activation=tf.tanh):
-    with tf.name_scope(scope) as scope_:
-        # Initialize the weights
-        state_size = input_shape[1]
-
-        w_hv = tf.get_variable('w_hv', [4 * num_units, 2 * num_units], initializer=initializer)
-        w_hz = tf.get_variable('w_hz', [4 * num_units, state_size], initializer=initializer)
-        w_vh = tf.get_variable('w_vh', [2 * num_units, num_units], initializer=initializer)
-        w_va = tf.get_variable('w_va', [2 * num_units, action_dim], initializer=initializer)
-
-        # init the cell
-        cell = MultiplicativeUnit(num_units, w_hv, w_hz, w_vh, w_va, forget_bias, activation)
-        # call the cell
-        if h is None:
-            h = cell.zero_state(tf.shape(x)[0], tf.float32)
-
-        output, state = cell(x, h, a)
-
-    return output, state
--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from layers import masked_conv2d
 
 
 class VideoPixelNetworkModel:
@@ -58,7 +59,64 @@ class VideoPixelNetworkModel:
 
             return mu
 
-    def residual_multiplicative_block(self, h, dilation_rate):
+    def multiplicative_unit_with_mask(self, h, mask_type, masked_channels, in_channels, out_channels):
+        with tf.variable_scope('multiplicative_unit'):
+            g1 = masked_conv2d(
+                h,
+                in_channels,  # num of channels in input
+                out_channels,
+                3,
+                mask_type,  # A, B or None
+                masked_channels,
+                padding='same',
+                activation=tf.sigmoid,
+                kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                name='g1')
+
+            g2 = masked_conv2d(
+                h,
+                in_channels,  # num of channels in input
+                out_channels,
+                3,
+                mask_type,  # A, B or None
+                masked_channels,
+                padding='same',
+                activation=tf.sigmoid,
+                kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                name='g2')
+
+            g3 = masked_conv2d(
+                h,
+                in_channels,  # num of channels in input
+                out_channels,
+                3,
+                mask_type,  # A, B or None
+                masked_channels,
+                padding='same',
+                activation=tf.sigmoid,
+                kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                name='g3')
+
+            u = masked_conv2d(
+                h,
+                in_channels,  # num of channels in input
+                out_channels,
+                3,
+                mask_type,  # A, B or None
+                masked_channels,
+                padding='same',
+                activation=tf.tanh,
+                kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                name='u')
+
+            g2_h = tf.multiply(g2, h)
+            g3_u = tf.multiply(g3, u)
+
+            mu = tf.multiply(g1, tf.tanh(g2_h + g3_u))
+
+            return mu
+
+    def residual_multiplicative_block_without_mask(self, h, dilation_rate):
         with tf.variable_scope('residual_multiplicative_block'):
             h1 = tf.layers.conv2d(
                 h,
@@ -88,23 +146,87 @@ class VideoPixelNetworkModel:
 
             return rmb
 
+    def residual_multiplicative_block_with_mask(self, h, first_block=False, last_block=False):
+        with tf.variable_scope('residual_multiplicative_block'):
+
+            h1 = tf.layers.conv2d(
+                h,
+                self.config.rmb_c,
+                1,
+                padding='same',
+                activation=None,
+                kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                name='h1'
+            )
+
+            if first_block:
+                h2 = self.multiplicative_unit_with_mask(
+                    h1, 'A',
+                    self.config.input_shape[2],
+                    self.config.input_shape[2] + self.config.rmb_c,
+                    self.config.rmb_c,
+                    self.config.rmb_c)
+            else:
+                h2 = self.multiplicative_unit_with_mask(
+                    h1, 'B',
+                    self.config.rmb_c,
+                    self.config.rmb_c,
+                    self.config.rmb_c)
+
+            h3 = self.multiplicative_unit_with_mask(
+                h2, 'B',
+                self.config.rmb_c,
+                self.config.rmb_c,
+                self.config.rmb_c)
+
+            if last_block:
+                h4 = tf.layers.conv2d(
+                    h3,
+                    255,
+                    1,
+                    padding='same',
+                    activation=None,
+                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                    name='h4'
+                )
+            else:
+                h4 = tf.layers.conv2d(
+                    h3,
+                    2 * self.config.rmb_c,
+                    1,
+                    padding='same',
+                    activation=None,
+                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                    name='h4'
+                )
+
+            rmb = tf.add(h, h4)
+
+            return rmb
+
     def resolution_preserving_cnn_encoders(self, x):
         with tf.variable_scope('resolution_preserving_cnn_encoders'):
             for i in range(self.config.encoder_rmb_num):
                 if self.config.encoder_rmb_dilation:
-                    x = self.residual_multiplicative_block(x, self.config.encoder_rmb_dilation_scheme[i])
+                    x = self.residual_multiplicative_block_without_mask(x, self.config.encoder_rmb_dilation_scheme[i])
                 else:
-                    x = self.residual_multiplicative_block(x, 1)
+                    x = self.residual_multiplicative_block_without_mask(x, 1)
 
             return x
 
     def pixel_cnn_decoders(self, h, x):
         with tf.variable_scope('pixel_cnn_decoders'):
-            self.residual_multiplicative_block(h, 1)
+            h = self.residual_multiplicative_block_without_mask(h, 1)
 
-            tf.concat([x, h], axis=3)
-            self.residual_multiplicative_block()
+            h = tf.concat([x, h], axis=3)
+            h = self.residual_multiplicative_block_with_mask(h, True)
 
+            for i in range(2, self.config.decoder_rmb_num - 1):
+                h = self.residual_multiplicative_block_with_mask(h)
+
+                h = self.residual_multiplicative_block_with_mask(h, last_block=True)
+
+            return h
 
     def conv_lstm(self):
         pass
