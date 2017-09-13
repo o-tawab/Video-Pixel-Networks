21065a0aa2b310a3a62b408d318a14ac6d71b5d2
Omar ABDELTAWAB (EXT) <omar.abdeltawab.ext@valeo.com>
2017-09-13 12:20:50 +0200

    smaller network

--- trainer.py
+++ trainer.py
@@ -92,7 +92,7 @@ class Trainer:
             self.logger.add_scalar_summary(self.global_step_tensor.eval(self.sess), {'train_loss': np.mean(losses)})
             self.sess.run(self.cur_epoch_assign_op, {self.cur_epoch_input: self.cur_epoch_tensor.eval(self.sess) + 1})
 
-            if epoch % 30 == 0:
+            if epoch % 100 == 0:
                 self.test()
                 self.save()
 
